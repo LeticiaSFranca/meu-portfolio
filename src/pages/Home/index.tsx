e5ee93803230bcb52317a8396af737afe608073e
LeticiaSFranca <leticiasilvafranca17@gmail.com>
2024-03-11 17:53:05 -0300

    Arrumando toda a estilização

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,10 +7,11 @@ export function Home() {
             <section id='primeira-apresentacao'>
                 <img src={imgApresentacao} alt="img-apresentação" id='img-apresentacao' />
                 <div id='texto-abertura'>
-                    <h3>Bem-vindo(a)!</h3>
-                    <p>Oie! Me chamo <b>Letícia França</b>, muito prazer! Navegue pelo meu portfólio: no cabeçalho tem duas opções de áreas que atuo,
-                        você pode escolher qual deseja visitar! E caso queira entrar em contato comigo, pode acessar 
-                        também pelo cabeçalho!
+                    <h3 id='titulo-apresentacao'>Bem-vindo(a)!</h3>
+                    <p>Oie! Me chamo <b id='meu-nome'>Letícia França</b>, muito prazer! <br /><br />
+                        Navegue pelo meu portfólio: no cabeçalho tem duas opções de áreas que atuo,
+                        você pode escolher qual deseja visitar🤎! E caso queira entrar em contato comigo, pode acessar 
+                        também pelo cabeçalho🤎!
                     </p>
                 </div>
             </section>
