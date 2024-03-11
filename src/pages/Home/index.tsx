1654b8165831a81756e85fad950cb289632d272b
LeticiaSFranca <leticiasilvafranca17@gmail.com>
2024-03-11 09:10:31 -0300

    Criação da 1º parte da apresentação

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,23 @@
 import './Home.style.css';
+import imgApresentacao from '../../assets/img/imagem-apresentação.png';
 
 export function Home() {
     return (
         <>
-            <h1>Home</h1>
+            <section id='primeira-apresentacao'>
+                <img src={imgApresentacao} alt="img-apresentação" id='img-apresentacao' />
+                <div id='texto-abertura'>
+                    <h3>Oie! Me chamo Letícia, muito prazer!</h3>
+                    <p>Seja bem-vindo(a) ao meu portfólio! No cabeçalho tem duas opções de áreas que atuo,
+                        você pode escolher qual deseja visitar! E caso queira entrar em contato comigo, pode acessar 
+                        também pelo cabeçalho!
+                    </p>
+                </div>
+            </section>
+
+            <section id='segunda-apresentacao'>
+                
+            </section>
         </>
     );
 }
