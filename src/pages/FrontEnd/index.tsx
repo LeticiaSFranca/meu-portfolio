3a2c4c77673ec6022bb344d618bba79e62305637
LeticiaSFranca <leticiasilvafranca17@gmail.com>
2024-03-20 19:09:08 -0300

    Update no Card-Projetos

--- src/pages/FrontEnd/index.tsx
+++ src/pages/FrontEnd/index.tsx
@@ -2,6 +2,7 @@ import './FrontEnd.style.css';
 import imgApresentacao from '../../assets/img/imagem-apresentação.png';
 import CardProjetos from '../../components/CardProjetos';
 import Imagem from '../../assets/icons/logo-cavalo.png'
+
 import Design1 from '../../assets/img/Projetos/Design/Design-1.png';
 import Design2 from '../../assets/img/Projetos/Design/Design-2.png';
 import Design3 from '../../assets/img/Projetos/Design/Design-3.png';
@@ -11,6 +12,13 @@ import Design6 from '../../assets/img/Projetos/Design/Design-6.png';
 import Design7 from '../../assets/img/Projetos/Design/Design-7.png';
 import Design8 from '../../assets/img/Projetos/Design/Design-8.png';
 import Design9 from '../../assets/img/Projetos/Design/Design-9.png';
+import Design10 from '../../assets/img/Projetos/Design/Design-10.png';
+
+import Dev1 from '../../assets/img/Projetos/Programação/Dev-1.png';
+import Dev2 from '../../assets/img/Projetos/Programação/Dev-2.png';
+import Dev3 from '../../assets/img/Projetos/Programação/Dev-3.png';
+import Dev4 from '../../assets/img/Projetos/Programação/Dev-4.png';
+import Dev5 from '../../assets/img/Projetos/Programação/Dev-5.png';
 
 export function FrontEnd() {
     return (
@@ -33,47 +41,52 @@ export function FrontEnd() {
                 <CardProjetos
                 imagemProjetos={Design1}
                 nomeProjetos='Calendários'
-                descricaoProjetos='Lorem ipsum, dolor sit amet consectetur adipisicing elit.' />
+                descricaoProjetos='Do jeitinho que você quiser!' />
                 
                 <CardProjetos
                 imagemProjetos={Design2}
-                nomeProjetos='Capas de Caderno/Agendas'
-                descricaoProjetos='Lorem ipsum, dolor sit amet consectetur adipisicing elit.' />
+                nomeProjetos='Capas de Cadernos'
+                descricaoProjetos='Do jeitinho que você quiser!' />
 
                 <CardProjetos
                 imagemProjetos={Design3}
                 nomeProjetos='Criação de Vídeos'
-                descricaoProjetos='Lorem ipsum, dolor sit amet consectetur adipisicing elit.' />
+                descricaoProjetos='Do jeitinho que você quiser!' />
 
                 <CardProjetos
                 imagemProjetos={Design4}
                 nomeProjetos='Criação de Imagens'
-                descricaoProjetos='Lorem ipsum, dolor sit amet consectetur adipisicing elit.' />
+                descricaoProjetos='Do jeitinho que você quiser!' />
 
                 <CardProjetos
                 imagemProjetos={Design5}
                 nomeProjetos='Adesivos'
-                descricaoProjetos='Lorem ipsum, dolor sit amet consectetur adipisicing elit.' />
+                descricaoProjetos='Do jeitinho que você quiser!' />
 
                 <CardProjetos
                 imagemProjetos={Design6}
                 nomeProjetos='Cartões de Visita'
-                descricaoProjetos='Lorem ipsum, dolor sit amet consectetur adipisicing elit.' />
+                descricaoProjetos='Do jeitinho que você quiser!' />
 
                 <CardProjetos
                 imagemProjetos={Design7}
                 nomeProjetos='Crachás'
-                descricaoProjetos='Lorem ipsum, dolor sit amet consectetur adipisicing elit.' />
+                descricaoProjetos='Do jeitinho que você quiser!' />
 
                 <CardProjetos
                 imagemProjetos={Design8}
                 nomeProjetos='Logomarcas'
-                descricaoProjetos='Lorem ipsum, dolor sit amet consectetur adipisicing elit.' />
+                descricaoProjetos='Do jeitinho que você quiser!' />
 
                 <CardProjetos
                 imagemProjetos={Design9}
                 nomeProjetos='Brasão'
-                descricaoProjetos='Lorem ipsum, dolor sit amet consectetur adipisicing elit.' />
+                descricaoProjetos='Do jeitinho que você quiser!' />
+
+                <CardProjetos
+                imagemProjetos={Design10}
+                nomeProjetos='Meu YouTube'
+                descricaoProjetos='Aqui ficam guardados todos os meus projetos em vídeos!' />
                 </div>
             </section>
 
@@ -82,33 +95,28 @@ export function FrontEnd() {
                 
                 <div className='todos-os-cards'>
                 <CardProjetos
-                imagemProjetos={Imagem}
-                nomeProjetos='Nome do Projeto'
-                descricaoProjetos='Lorem ipsum, dolor sit amet consectetur adipisicing elit.' />
+                imagemProjetos={Dev1}
+                nomeProjetos='CraftAPI'
+                descricaoProjetos='Proposta de projeto de estudos, produzir uma API e usá-la com o tema que quiséssemos!' />
                 
                 <CardProjetos
-                imagemProjetos={Imagem}
-                nomeProjetos='Nome do Projeto'
+                imagemProjetos={Dev2}
+                nomeProjetos='To-Do List'
+                descricaoProjetos='Proposta de projeto de estudos, produzir uma lista de tarefas. Onde vc seleciona os pendentes e os concluídos.' />
+
+                <CardProjetos
+                imagemProjetos={Dev3}
+                nomeProjetos='WE Equipamentos'
                 descricaoProjetos='Lorem ipsum, dolor sit amet consectetur adipisicing elit.' />
 
                 <CardProjetos
-                imagemProjetos={Imagem}
-                nomeProjetos='Nome do Projeto'
+                imagemProjetos={Dev4}
+                nomeProjetos='QuickFood'
                 descricaoProjetos='Lorem ipsum, dolor sit amet consectetur adipisicing elit.' />
 
                 <CardProjetos
-                imagemProjetos={Imagem}
-                nomeProjetos='Nome do Projeto'
-                descricaoProjetos='Lorem ipsum, dolor sit amet consectetur adipisicing elit.' />
-
-                <CardProjetos
-                imagemProjetos={Imagem}
-                nomeProjetos='Nome do Projeto'
-                descricaoProjetos='Lorem ipsum, dolor sit amet consectetur adipisicing elit.' />
-
-                <CardProjetos
-                imagemProjetos={Imagem}
-                nomeProjetos='Nome do Projeto'
+                imagemProjetos={Dev5}
+                nomeProjetos='Meu GitHub'
                 descricaoProjetos='Lorem ipsum, dolor sit amet consectetur adipisicing elit.' />
                 </div>
             </section>
