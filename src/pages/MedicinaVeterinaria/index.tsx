e0ecc6e091438ea8e9804d4a549b98559c3ed14b
LeticiaSFranca <leticiasilvafranca17@gmail.com>
2024-03-20 22:47:01 -0300

    Começando cards de MedVet

--- src/pages/MedicinaVeterinaria/index.tsx
+++ src/pages/MedicinaVeterinaria/index.tsx
@@ -1,9 +1,12 @@
 import './MedicinaVeterinaria.style.css';
 import imgApresentacao from '../../assets/img/imagem-apresentação-2.png';
+import Imagem from '../../assets/img/em-andamento.png';
+import CardProjetos from '../../components/CardProjetos';
 
 export function MedicinaVeterinaria() {
     return (
-        <section id='primeira-apresentacao'>
+        <>
+            <section id='primeira-apresentacao'>
                 <img src={imgApresentacao} alt="img-apresentação" id='img-apresentacao' />
                 <div id='texto-abertura'>
                     <h3 id='titulo-apresentacao'>Medicina Veterinária</h3>
@@ -12,6 +15,38 @@ export function MedicinaVeterinaria() {
                         ut deserunt veniam blanditiis. Iure exercitationem blanditiis 
                         numquam iusto nisi necessitatibus veritatis voluptatem illum?</p>
                 </div>
-        </section>
+            </section>
+
+            <section id='segunda-apresentacao'>
+                <h2>Cursos e Congressos</h2>
+                
+                <div className='todos-os-cards'>
+                <CardProjetos
+                imagemProjetos={Imagem}
+                nomeProjetos='Em Breve'
+                descricaoProjetos='...' />
+                
+                <CardProjetos
+                imagemProjetos={Imagem}
+                nomeProjetos='Em Breve'
+                descricaoProjetos='...' />
+
+                <CardProjetos
+                imagemProjetos={Imagem}
+                nomeProjetos='Em Breve'
+                descricaoProjetos='...' />
+
+                <CardProjetos
+                imagemProjetos={Imagem}
+                nomeProjetos='Em Breve'
+                descricaoProjetos='...' />
+
+                <CardProjetos
+                imagemProjetos={Imagem}
+                nomeProjetos='Em Breve'
+                descricaoProjetos='...' />
+                </div>
+            </section>
+        </>
     );
 }
