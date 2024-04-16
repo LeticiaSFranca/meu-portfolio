b670831bcf63fad4a9b809441020d9841a9e57c4
LeticiaSFranca <leticiasilvafranca17@gmail.com>
2024-04-15 23:07:50 -0300

    Fazendo página 404

--- src/pages/Page404/index.tsx
+++ src/pages/Page404/index.tsx
@@ -1,4 +1,7 @@
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './Page404.style.css';
+import img404 from '../../assets/img/imagem-404.png';
 
 export function Page404() {
     useEffect(() => {
@@ -7,8 +10,14 @@ export function Page404() {
     
     return (
         <>
-            <h1>Erro 404</h1>
-            <p>Página não encontrada</p>
+            <section id='page-404'>
+                <img src={img404} alt="imagem-404" id='img-404'/>
+                
+                <div id='div-404'>
+                    <p id='texto-404'>Ops...algo não deu certo!</p>
+                    <Link to='/' id='botao-404'>Voltar ao início</Link>
+                </div>
+            </section>
         </>
     );
 }
