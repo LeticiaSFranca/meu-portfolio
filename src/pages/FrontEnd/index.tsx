5c877ef4aad0b0e07b42f5d14074137463eff62a
LeticiaSFranca <leticiasilvafranca17@gmail.com>
2024-08-30 12:01:07 -0300

    Update #2 em Imagens de Projetos

--- src/pages/FrontEnd/index.tsx
+++ src/pages/FrontEnd/index.tsx
@@ -12,7 +12,6 @@ import Design5 from '../../assets/img/Projetos/Design/Design-5.png';
 import Design6 from '../../assets/img/Projetos/Design/Design-6.png';
 import Design7 from '../../assets/img/Projetos/Design/Design-7.png';
 import Design8 from '../../assets/img/Projetos/Design/Design-8.png';
-import Design9 from '../../assets/img/Projetos/Design/Design-9.png';
 import Design10 from '../../assets/img/Projetos/Design/Design-10.png';
 import Design11 from '../../assets/img/Projetos/Design/Design-11.png';
 import Design12 from '../../assets/img/Projetos/Design/Design-12.png';
@@ -21,13 +20,11 @@ import Dev1 from '../../assets/img/Projetos/Programação/Dev-1.png';
 import Dev2 from '../../assets/img/Projetos/Programação/Dev-2.png';
 import Dev3 from '../../assets/img/Projetos/Programação/Dev-3.png';
 import Dev4 from '../../assets/img/Projetos/Programação/Dev-4.png';
+import Dev5 from '../../assets/img/Projetos/Programação/Dev-5.png';
 import Dev6 from '../../assets/img/Projetos/Programação/Dev-6.png';
 import Dev7 from '../../assets/img/Projetos/Programação/Dev-7.png';
 import Dev8 from '../../assets/img/Projetos/Programação/Dev-8.png';
 import Dev9 from '../../assets/img/Projetos/Programação/Dev-9.png';
-import Dev10 from '../../assets/img/Projetos/Programação/Dev-10.png';
-import Dev11 from '../../assets/img/Projetos/Programação/Dev-11.png';
-import Dev12 from '../../assets/img/Projetos/Programação/Dev-12.png';
 
 import { FaHtml5 } from 'react-icons/fa';
 import { IoLogoCss3 } from 'react-icons/io';
@@ -109,7 +106,7 @@ export function FrontEnd() {
 
                     <CardProjetos
                         imagemProjetos={Design6}
-                        nomeProjetos="Adesivos"
+                        nomeProjetos="Adesivos & Logos"
                         linkProjetos=""
                         nomeBotao=""
                     />
@@ -124,13 +121,6 @@ export function FrontEnd() {
                     <CardProjetos
                         imagemProjetos={Design8}
                         nomeProjetos="Crachás"
-                        linkProjetos=""
-                        nomeBotao=""
-                    />
-
-                    <CardProjetos
-                        imagemProjetos={Design9}
-                        nomeProjetos="Logomarcas"
                         linkProjetos=""
                         nomeBotao=""
                     />
@@ -164,81 +154,67 @@ export function FrontEnd() {
                 <div className="todos-os-cards">
                     <CardProjetos
                         imagemProjetos={Dev1}
-                        nomeProjetos="CraftAPI"
-                        linkProjetos="https://craft-api.vercel.app"
-                        nomeBotao="> Acessar <"
+                        nomeProjetos="WE Equipamentos"
+                        linkProjetos="https://weequipamentos.com.br"
+                        nomeBotao="➡ Acessar"
                     />
 
                     <CardProjetos
                         imagemProjetos={Dev2}
-                        nomeProjetos="To-Do List"
-                        linkProjetos="https://to-do-list-framework.vercel.app/"
-                        nomeBotao="> Acessar <"
+                        nomeProjetos="Projeto Criatório Comercial (Fictício)"
+                        linkProjetos="https://plumaspreciosas.vercel.app/"
+                        nomeBotao="➡ Acessar"
                     />
 
                     <CardProjetos
                         imagemProjetos={Dev3}
-                        nomeProjetos="WE Equipamentos"
-                        linkProjetos="https://weequipamentos.com.br"
-                        nomeBotao="> Acessar <"
+                        nomeProjetos="Projeto AngellsVet"
+                        linkProjetos="https://angellsvet.vercel.app/"
+                        nomeBotao="➡ Acessar"
                     />
 
                     <CardProjetos
                         imagemProjetos={Dev4}
-                        nomeProjetos="QuickFood"
-                        linkProjetos="https://qftech.com.br"
-                        nomeBotao="> Acessar <"
+                        nomeProjetos="Projeto MindWell"
+                        linkProjetos="https://projetomindwell.vercel.app/"
+                        nomeBotao="➡ Acessar"
+                    />
+
+                    <CardProjetos
+                        imagemProjetos={Dev5}
+                        nomeProjetos="CraftAPI"
+                        linkProjetos="https://craft-api.vercel.app"
+                        nomeBotao="➡ Acessar"
                     />
 
                     <CardProjetos
                         imagemProjetos={Dev6}
-                        nomeProjetos="Gerenciamento de Estoque"
-                        linkProjetos="https://github.com/LeticiaSFranca/Projetos-Html-Css/tree/main/projeto-estoque-js"
-                        nomeBotao="> GitHub <"
+                        nomeProjetos="Landing Page - Super Mario Bross"
+                        linkProjetos="https://github.com/LeticiaSFranca/Projetos-Html-Css/tree/main/site-mario-bross"
+                        nomeBotao="➡ GitHub"
                     />
 
                     <CardProjetos
                         imagemProjetos={Dev7}
-                        nomeProjetos="E-Commerce Estático"
-                        linkProjetos="https://github.com/LeticiaSFranca/Projetos-Html-Css/tree/main/E-Commerce-main"
-                        nomeBotao="> GitHub <"
+                        nomeProjetos="Projeto Listify - Lista de Tarefas"
+                        linkProjetos="https://listify-spring-boot.vercel.app/"
+                        nomeBotao="➡ Acessar"
                     />
 
                     <CardProjetos
                         imagemProjetos={Dev8}
-                        nomeProjetos="Landing Page - Super Mario Bross"
-                        linkProjetos="https://github.com/LeticiaSFranca/Projetos-Html-Css/tree/main/site-mario-bross"
-                        nomeBotao="> GitHub <"
+                        nomeProjetos="API das Olimíadas"
+                        linkProjetos="https://pop24.vercel.app/"
+                        nomeBotao="➡ Acessar"
                     />
 
                     <CardProjetos
                         imagemProjetos={Dev9}
-                        nomeProjetos="Meu Primeiro Site em HTML & CSS"
-                        linkProjetos="https://github.com/LeticiaSFranca/Projetos-Html-Css/tree/main/Meu-Primeiro-Site"
-                        nomeBotao="> GitHub <"
+                        nomeProjetos="Space for Development"
+                        linkProjetos="https://spacefordevelopment.vercel.app/"
+                        nomeBotao="➡ Acessar"
                     />
-
-                    <CardProjetos
-                        imagemProjetos={Dev10}
-                        nomeProjetos="Projeto WindWell"
-                        linkProjetos="https://projetomindwell.vercel.app/"
-                        nomeBotao="> Acessar <"
-                    />
-
-                    <CardProjetos
-                        imagemProjetos={Dev11}
-                        nomeProjetos="Plumas Preciosas"
-                        linkProjetos="https://plumaspreciosas.vercel.app/"
-                        nomeBotao="> Acessar <"
-                    />
-
-                    <CardProjetos
-                        imagemProjetos={Dev12}
-                        nomeProjetos="Angells Vet"
-                        linkProjetos="https://angellsvet.vercel.app/"
-                        nomeBotao="> Acessar <"
-                    />
-                </div>
+                    </div>
             </section>
 
             <section id="secao-registros-frontend">
